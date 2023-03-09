ab00fac78b1d27885fc913d1c87c32eb8b2e3e61
CatherineStone <106274539+CatherineStone@users.noreply.github.com>
2023-03-09 15:39:52 +0000

    stuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import { useState } from "react";
+
 import "./App.css"
 
 import UserWrapper from "./components/userWrapper/UserWrapper";
 
 const App = () => {
+
+  const [user, setUser] = useState({
+    username: null,
+    email: null,
+    token: null,
+  });
+
+
   return (
     <div className="app-wrapper">
       <h1>Awesome App!</h1>
-      <UserWrapper />
+      <UserWrapper user={user} setUser={setUser} />
+
+      <>{user.token ? (
+        <p>{user.username} is logged in</p> 
+      ) : (
+          <p>{user.username} is not logged in</p>
+        )}
+      </>
     </div>
   );
 }
--- src/components/addUser/AddUser.js
+++ src/components/addUser/AddUser.js
@@ -1,20 +1,43 @@
 import React from "react"
 import "./AddUser.css"
+import { useState } from "react";
 
-const AddUser = () => {
+import { addUser } from "../../utils"
+
+const AddUser = ({ user, setUser }) => {
+    const [username, setUsername] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const onRegisterSubmit = async (e) => {
+        e.preventDefault();
+        console.log(username, email, password);
+
+        await addUser(username, email, password);
+    };
+
     return (
         <div className="adduser-wrapper">
-            <form className="adduser-form"> 
+            <form className="adduser-form" onSubmit={onRegisterSubmit}> 
                 <label> Username:
-                    <input placeholder="username" />
+                    <input
+                    placeholder="username" 
+                    onChange={(e) => setUsername(e.target.value)} 
+                    />
                 </label>
                 <label> Email:
-                    <input placeholder="email" />
+                    <input 
+                    placeholder="email" 
+                    onChange={(e) => setEmail(e.target.value)} 
+                    />
                 </label>
                 <label> Password:
-                    <input placeholder="password" />
+                    <input 
+                    placeholder="password" 
+                    onChange={(e) => setPassword(e.target.value)} 
+                    />
                 </label>
-                <button>Register</button>
+                <button type="submit">Register</button>
             </form>
         </div>
     );
